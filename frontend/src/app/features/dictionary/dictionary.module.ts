a9dee67eb0b2a3e6c38b45a2abf08913999fd13a
Chermann-KING <codes.smiling@gmail.com>
2025-03-29 07:56:34 +0100

    🚧 fontend: Met à jour le module

--- frontend/src/app/features/dictionary/dictionary.module.ts
+++ frontend/src/app/features/dictionary/dictionary.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DictionaryRoutingModule } from './dictionary-routing.module';
+import { SharedModule } from '../../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
+import { SearchComponent } from './components/search/search.component';
+import { WordDetailsComponent } from './components/word-details/word-details.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { FavoriteWordsComponent } from './components/favorite-words/favorite-words.component';
+import { WordCardComponent } from './components/word-card/word-card.component';
+import { AddWordComponent } from './components/add-word/add-word.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SearchComponent,
+    WordDetailsComponent,
+    SearchResultsComponent,
+    FavoriteWordsComponent,
+    WordCardComponent,
+    AddWordComponent,
+  ],
   imports: [
     CommonModule,
-    DictionaryRoutingModule
-  ]
+    FormsModule,
+    ReactiveFormsModule,
+    DictionaryRoutingModule,
+    SharedModule,
+    RouterModule,
+  ],
+  exports: [
+    SearchComponent,
+    WordDetailsComponent,
+    WordCardComponent,
+    SearchResultsComponent,
+  ],
 })
-export class DictionaryModule { }
+export class DictionaryModule {}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -7,9 +7,17 @@ import { FooterComponent } from './components/footer/footer.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { MainNavComponent } from './components/main-nav/main-nav.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { CustomDropdownComponent } from './components/custom-dropdown/custom-dropdown.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, LogoComponent, MainNavComponent, ProfileComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    MainNavComponent,
+    ProfileComponent,
+    CustomDropdownComponent,
+  ],
   imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
   exports: [
     CommonModule,
@@ -18,6 +26,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     RouterModule,
     HeaderComponent,
     FooterComponent,
+    CustomDropdownComponent,
   ],
 })
 export class SharedModule {}
